5fd71b4786d7ac72b19b16a2722b76247cc03d95
jerinjoseph2468 <118135141+jerinjoseph2468@users.noreply.github.com>
2022-11-13 16:36:14 -0700

    This class will control the processing Agents

---
+++ pizza.java
@@ -0,0 +1,25 @@
+package application;
+
+public class pizza {
+
+	String pizzaType = "";
+	String pizzaTopping = "";
+	
+	public void setPizzaType(String pizzaType1)
+	{
+		this.pizzaType = pizzaType1;
+	}
+	public void setPizzaTopping(String pizzaTopping1)
+	{
+		this.pizzaTopping = pizzaTopping1;
+	}
+	public String getPizzaType()
+	{
+		return pizzaType;
+	}
+	public String getPizzaTopping()
+	{
+		return pizzaTopping;
+	}
+
+}
